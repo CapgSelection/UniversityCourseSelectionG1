bcbf89d355b4eef0d70d5f3f0b7952cb5df54d67
Admin <Admin@192.168.0.104>
2022-10-31 16:43:55 +0530

    applicant commit2

--- src/main/java/com/example/UniversityCourseSelectionG1/service/ApplicantService.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/service/ApplicantService.java
@@ -1,9 +1,17 @@
 package com.example.UniversityCourseSelectionG1.service;
 
+import java.util.Optional;
+
 import com.example.UniversityCourseSelectionG1.entities.Applicant;
 
 public interface ApplicantService {
 
 	Applicant applyForCourse(Applicant apl);
+
+	Optional<Applicant> getById(int applicantId);
+
+	Applicant updateApplicant(Applicant applicant);
+
+	void delApplicant(Applicant applicant);
 
 }
