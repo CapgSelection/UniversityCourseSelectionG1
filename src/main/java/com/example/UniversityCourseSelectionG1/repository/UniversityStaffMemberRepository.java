ca72ec47b5373ed5d1ad3630f960a6e336231f1b
user <user@DESKTOP-VBG7F0V>
2022-10-31 16:53:15 +0530

    commit

--- src/main/java/com/example/UniversityCourseSelectionG1/repository/UniversityStaffMemberRepository.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/repository/UniversityStaffMemberRepository.java
@@ -1,6 +1,10 @@
 package com.example.UniversityCourseSelectionG1.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.UniversityCourseSelectionG1.entities.UniversityStaffMember;
@@ -8,4 +12,11 @@ import com.example.UniversityCourseSelectionG1.entities.UniversityStaffMember;
 @Repository
 public interface UniversityStaffMemberRepository extends JpaRepository<UniversityStaffMember, Integer> {
 
+	@Query(value="select * from university_staff_member where staff_Id=?1 and password=?2",nativeQuery = true)
+	UniversityStaffMember verifyUniversityStaffMemberCredentials(int id,String password);
+	
+	@Transactional
+	@Modifying
+	@Query(value="ALTER SEQUENCE staff_sequence RESTART WITH 1", nativeQuery = true)
+	void resetStaffIdSequence();
 }
--- src/main/java/com/example/UniversityCourseSelectionG1/service/UniversityStaffMemberServiceImpl.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/service/UniversityStaffMemberServiceImpl.java
@@ -1,5 +1,84 @@
 package com.example.UniversityCourseSelectionG1.service;
 
-public class UniversityStaffMemberServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.UniversityCourseSelectionG1.entities.Course;
+import com.example.UniversityCourseSelectionG1.entities.UniversityStaffMember;
+import com.example.UniversityCourseSelectionG1.exception.NotFoundException;
+import com.example.UniversityCourseSelectionG1.repository.UniversityStaffMemberRepository;
+
+ @Service
+public class UniversityStaffMemberServiceImpl implements UniversityStaffMemberService {
+	
+	 @Autowired
+	private UniversityStaffMemberRepository staffRepo;
+	@Autowired
+	private CourseService courseService;
+	
+	@Override
+	public UniversityStaffMember addStaff(UniversityStaffMember usm) {
+		return staffRepo.save(usm);
+	}
+
+	@Override
+	public UniversityStaffMember updateStaff(UniversityStaffMember usm) {
+		if(staffRepo.existsById(usm.getStaffId())) {
+			return staffRepo.save(usm);
+		}
+		else {
+			throw new NotFoundException("Staff with id: "+usm.getStaffId()+" not found!");
+		}
+	}
+
+	@Override
+	public UniversityStaffMember viewStaff(int id) {
+		if(staffRepo.existsById(id)) {
+			return staffRepo.getReferenceById(id);
+		}
+		else {
+			throw new NotFoundException("Staff with id: "+id+" not found!");
+		}
+	}
+
+	@Override
+	public void removeStaff(int id) {
+		if(staffRepo.existsById(id)) {
+			if(staffRepo.count()==1) {
+				throw new NotFoundException("Cannot delete last record");
+			}
+			else {
+				staffRepo.deleteById(id);
+			}
+		}
+		else {
+			throw new NotFoundException("Staff with id: "+id+" not found!");
+		}	
+	}
+
+	@Override
+	public List<UniversityStaffMember> viewAllStaffs() {
+		List<UniversityStaffMember> usmList = staffRepo.findAll();
+		if(usmList.isEmpty())
+			throw new NotFoundException("No staff records found!");
+		return usmList;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		return courseService.addCourse(course);
+	}
+
+	@Override
+	public Course removeCourse(Course course) {
+		return courseService.removeCourse(course.getCourseId());
+	}
+
+	@Override
+	public Course updateCourse(Course course) {
+		return courseService.updateCourse(course);
+	}
+  
 }
