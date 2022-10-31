9ac4c16726363b744a0b8dc2b8d13d0bfdb082b6
amanm <amanm@DemonSlasher>
2022-10-31 12:34:32 +0530

    Updating pom.xml and admissionComiteeMember

--- src/main/java/com/example/UniversityCourseSelectionG1/entities/AdmissionCommiteeMember.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/entities/AdmissionCommiteeMember.java
@@ -1,5 +1,23 @@
 package com.example.UniversityCourseSelectionG1.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class AdmissionCommiteeMember {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int adminId;
+	private String adminName;
+	private String adminContact;
+	private String adminUsername;
+	private String adminPassword;
 }
