d71d3f95c3a6725069c1621dbb3795b1ad779cdf
CapgSelection <capgsprints@gmail.com>
2022-10-31 14:48:16 +0530

    Added Course Controller,Service,Repo..

---
+++ src/main/java/com/example/UniversityCourseSelectionG1/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.UniversityCourseSelectionG1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.UniversityCourseSelectionG1.entities.Course;
+
+public interface CourseRepository extends JpaRepository<Course,Integer>{
+
+}
