22b04b66921fb8c46ffe401cd919225ac89d5fa2
user <user@DESKTOP-VBG7F0V>
2022-10-31 15:48:57 +0530

    added

--- src/main/java/com/example/UniversityCourseSelectionG1/repository/UniversityStaffMemberRepository.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/repository/UniversityStaffMemberRepository.java
@@ -1,5 +1,11 @@
 package com.example.UniversityCourseSelectionG1.repository;
 
-public interface UniversityStaffMemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.UniversityCourseSelectionG1.entities.UniversityStaffMember;
+
+@Repository
+public interface UniversityStaffMemberRepository extends JpaRepository<UniversityStaffMember, Integer> {
 
 }
