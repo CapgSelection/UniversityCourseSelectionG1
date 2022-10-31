b6a6d307f892762a9facbd152e704ac9cbb725a6
amanm <amanm@DemonSlasher>
2022-10-31 15:20:03 +0530

    adding exception desc

---
+++ src/main/java/com/example/UniversityCourseSelectionG1/exception/ExceptionDescription.java
@@ -0,0 +1,13 @@
+package com.example.UniversityCourseSelectionG1.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDescription {
+
+	String message;
+}
