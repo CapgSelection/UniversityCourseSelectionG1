f56acccadfd6a2ddfde21f8ed9af7cc65c88313b
sanju <sanju@DESKTOP-VVA9UGP>
2022-10-31 15:08:51 +0530

    adding UniversityStaffMember entity

--- src/main/java/com/example/UniversityCourseSelectionG1/entities/UniversityStaffMember.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/entities/UniversityStaffMember.java
@@ -1,5 +1,36 @@
 package com.example.UniversityCourseSelectionG1.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class UniversityStaffMember {
+
+	@Id
+	@Column(name = "staff_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, 
+					generator = "staff_sequence")
+	@SequenceGenerator(name = "staff_sequence", 
+						sequenceName = "staff_sequence", 
+						allocationSize = 1)
+	private Integer staffId;
+	@Column(name = "staff_name")
+	private String staffName;
+	private String password;
+	private String role;
 
 }
