dd0558e87934dca87d0c9d0b1e3f1c652c2435f3
Tuba Fatma <tubafatma913@gmail.com>
2022-10-31 15:28:02 +0530

    admission

---
+++ src/main/java/com/example/UniversityCourseSelectionG1/repository/AdmissionRepository.java
@@ -0,0 +1,9 @@
+package com.example.UniversityCourseSelectionG1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.UniversityCourseSelectionG1.entities.Admission;
+
+public interface AdmissionRepository extends JpaRepository<Admission,Integer>{
+
+}
