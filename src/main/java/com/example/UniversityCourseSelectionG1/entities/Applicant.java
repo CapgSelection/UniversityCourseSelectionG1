920e051b9b44f44c9255cc6a398b8b8a25c80684
Admin <Admin@192.168.0.104>
2022-11-01 17:55:32 +0530

    commit5

--- src/main/java/com/example/UniversityCourseSelectionG1/entities/Applicant.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/entities/Applicant.java
@@ -1,12 +1,12 @@
 package com.example.UniversityCourseSelectionG1.entities;
 
-import javax.persistence.CascadeType;
+//import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+//import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
--- src/main/java/com/example/UniversityCourseSelectionG1/exception/ExceptionDescription.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/exception/ExceptionDescription.java
@@ -2,9 +2,12 @@ package com.example.UniversityCourseSelectionG1.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExceptionDescription {
