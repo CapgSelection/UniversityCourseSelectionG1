8c8cf9b477c5e3668dfb06fefb71ab69283162d8
user <user@DESKTOP-VBG7F0V>
2022-11-01 17:50:35 +0530

    Added

--- src/main/java/com/example/UniversityCourseSelectionG1/repository/ApplicantRepository.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/repository/ApplicantRepository.java
@@ -11,5 +11,10 @@ public interface ApplicantRepository extends JpaRepository<Applicant, Integer>{
 
 	@Query(value = "select * from Applicant where status=?1", nativeQuery = true)
 	List<Applicant> viewAllApplicantByStatus(int status);
+	
+	@Query(value="select * from Applicant where Applicant ID=?1 and Password=?2",nativeQuery = true)
+	Applicant verifyApplicantCredentials(int id, String password);
+
+	
 
 }
