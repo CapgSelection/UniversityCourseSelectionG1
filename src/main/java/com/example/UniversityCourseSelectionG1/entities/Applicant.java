c43809451b8882fd88702a2d4a2afb52ba031e9e
Tuba Fatma <tubafatma913@gmail.com>
2022-11-02 15:56:03 +0530

    commit

--- src/main/java/com/example/UniversityCourseSelectionG1/entities/Applicant.java
+++ src/main/java/com/example/UniversityCourseSelectionG1/entities/Applicant.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 //import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -41,6 +43,10 @@ public class Applicant {
 	@OneToOne
 
 	Admission admission;
+	@ManyToOne
+    @JoinColumn(name = "admissioncommiteeMember_id")
+    AdmissionCommiteeMember admissionCommiteeMember;
+	
 	AdmissionStatus status;
 
 
