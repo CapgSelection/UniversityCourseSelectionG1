a997e90134a92c4acaa49d7ad55a33dc654f3115
amanm <amanm@DemonSlasher>
2022-11-02 12:21:56 +0530

    auth

---
+++ src/main/java/com/example/UniversityCourseSelectionG1/entities/Authorization.java
@@ -0,0 +1,14 @@
+package com.example.UniversityCourseSelectionG1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authorization {
+
+	private int id;
+	private String pass;
+}
